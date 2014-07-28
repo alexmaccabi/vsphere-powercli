62677ec9419e3868bca06e4eb305a0735ce57ae3
AlexMaccabi <alex.maccabi@gmail.com>
2014-07-28 16:52:04 +0300

    Added change batch subnetmask

---
+++ ChangeSubnetMaskVM.ps1
@@ -0,0 +1,5 @@
+﻿Connect-VIServer -Server vcenter
+$vms = Get-Cluster cluster-name | Get-VM
+foreach($vm in $vms) {
+Get-VMGuestNetworkInterface $vm  -GuestCredential $guestcred | where {$_.SubnetMask -eq "255.255.0.0"} 
+Set-VMGuestNetworkInterface -Netmask 255.255.252.0 }
