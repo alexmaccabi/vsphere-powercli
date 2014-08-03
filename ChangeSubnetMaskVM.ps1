f49688b2c2a4571895e1fd82e59750996e146165
AlexMaccabi <alex.maccabi@gmail.com>
2014-08-03 15:51:10 +0300

    Had some problem passing vm name to Get-VMGeust function, and added get-cred for authentication

--- ChangeSubnetMaskVM.ps1
+++ ChangeSubnetMaskVM.ps1
@@ -1,5 +1,6 @@
 ﻿Connect-VIServer -Server vcenter
-$vms = Get-Cluster cluster-name | Get-VM
+$guestcred = get-credential
+$vms = Get-Cluster cluster-name | Get-VM 
 foreach($vm in $vms) {
 Get-VMGuestNetworkInterface $vm  -GuestCredential $guestcred | where {$_.SubnetMask -eq "255.255.0.0"} 
-Set-VMGuestNetworkInterface -Netmask 255.255.252.0 }
+Set-VMGuestNetworkInterface -GuestCredential $guestcred -Netmask 255.255.252.0 }
